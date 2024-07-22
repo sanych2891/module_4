ee9cc1be381043b9306176da5fded77e12c77b14
sanych2891 <sanych2891@icloud.com>
2024-07-22 13:14:12 +1000

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    return first / second
---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as new_fake
+from true_math import divide as new_true
+
+result1 = new_fake(69, 3)
+result2 = new_fake(3, 0)
+result3 = new_true(49, 7)
+result4 = new_true(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
